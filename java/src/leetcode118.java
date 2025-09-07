c807ddea0a24a8b5cc73eddb2cc856a44a9206ce
juno <laegel1@gmail.com>
2025-09-08 04:29:05 +0900

    add files

---
+++ java/src/leetcode118.java
@@ -0,0 +1,7 @@
+/**
+ * @author laege
+ * @version 1.0
+ * @since 2025-09-08
+ */
+public class leetcode118 {
+}
---
+++ java/src/leetcode70.java
@@ -0,0 +1,25 @@
+/**
+ * @author laege
+ * @version 1.0
+ * @since 2025-09-08
+ */
+public class leetcode70 {
+    public static int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(5));
+    }
+}
