2480c516ed900468cb6c85c9b37a5ae40e8f847c
Juno BAEK <laegel1@gmail.com>
2025-10-26 19:43:42 -0700

    solve leetcode 347

--- python/leetcode/leetcode347.py
+++ python/leetcode/leetcode347.py
@@ -18,7 +18,29 @@ class Solution:
             res.append(arr.pop()[1])
         return res
 
+    # bucket sort
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        cnt_dict = {}
+        for num in nums:
+            n = cnt_dict.get(num, 0) + 1
+            cnt_dict[num] = n
+
+        sort_list = [[] for i in range(len(nums) + 1)]
+
+        for n, c in cnt_dict.items():
+            sort_list[c].append(n)
+
+        result_list = []
+        for i in range(len(sort_list) - 1, 0, -1):
+            for num in sort_list[i]:
+                result_list.append(num)
+                if len(result_list) == k:
+                    return result_list
+
+
+        return result_list
+
 if __name__ == "__main__":
     s = Solution()
 
-    print(s.topKFrequent([1,2,2,3,3,3], 2))
+    print(s.topKFrequent2([1,2], 2))
