ccd51a6d609ada3365f14cf828bdb019549d44c4
Juno BAEK <laegel1@gmail.com>
2025-10-27 20:04:58 -0700

    solve leetcode 238

--- python/leetcode/leeetcode238.py
+++ python/leetcode/leeetcode238.py
@@ -25,7 +25,27 @@ class Solution:
 
         return res
 
+    # prefix & suffix
+    def productExceptSelf2(self, nums: List[int]) -> List[int]:
+        pref = [1] * len(nums)
+        suff = [1] * len(nums)
+        res = [0] * len(nums)
+
+        for i in range(1, len(nums)):
+            pref[i] = pref[i - 1] * nums[i - 1]
+
+        for i in range(len(nums) - 2, -1, -1):
+            suff[i] = suff[i + 1] * nums[i + 1]
+
+        for i in range(len(nums)):
+            res[i] = pref[i] * suff[i]
+
+        return res
+
+
+
 
 if __name__ == "__main__":
     s = Solution()
     print(s.productExceptSelf([1,2,3,4]))
+    print(s.productExceptSelf2([1,2,3,4]))
