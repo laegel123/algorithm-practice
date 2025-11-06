91d98bcc7dee425b8e78252c0f59ff2bb9dd1d32
Juno BAEK <laegel1@gmail.com>
2025-11-05 17:42:24 -0800

    solve leetcode 15

---
+++ python/leetcode/leetcode15.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i, n in enumerate(nums):
+            if n > 0:
+                break
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            l = i + 1
+            r = len(nums) - 1
+
+            while(l < r):
+                three_sum = n + nums[l] + nums[r]
+
+                if three_sum > 0:
+                    r -= 1
+                elif three_sum < 0:
+                    l += 1
+                else:
+                    res.append([n, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while(l < r and nums[l] == nums[l - 1]):
+                        l += 1
+
+        return res
