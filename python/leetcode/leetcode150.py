4f9949d72e786503b7969b63d22dfdafdb1c6559
Juno BAEK <laegel1@gmail.com>
2025-11-27 15:52:46 -0800

    solve leetcode 150

---
+++ python/leetcode/leetcode150.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif token == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif token == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif token == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(token))
+        return stack[0]
+
+
+
+if __name__ == "__main__":
+    s = Solution()
+
+    print(s.evalRPN(["1","2","+","3","*","4","-"]))
+
