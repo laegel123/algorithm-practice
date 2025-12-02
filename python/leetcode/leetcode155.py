31e50e35cf05a44f3f6ba22e19d301d119043ee0
juno <laegel1@gmail.com>
2025-12-02 11:08:27 -0800

    add files

---
+++ python/leetcode/leetcode155.py
@@ -0,0 +1,24 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        mini = self.stack[-1]
+
+        for i in range(0, len(self.stack)):
+            mini = min(mini, self.stack[i])
+
+        return mini
+
+
