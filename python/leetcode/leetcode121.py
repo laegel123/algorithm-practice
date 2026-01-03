2af477235e1a74772cd1da078f3c03e7f413fabe
juno <laegel1@gmail.com>
2026-01-02 16:07:07 -0800

    add files

--- python/leetcode/leetcode121.py
+++ python/leetcode/leetcode121.py
@@ -3,8 +3,8 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        mini = None;
-        prof = 0;
+        mini = None
+        prof = 0
 
         for n in prices:
             if mini is None:
@@ -15,3 +15,16 @@ class Solution:
                 prof = max(prof, n - mini)
 
         return prof
+
+    def maxProfit_twopointer(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        max_price = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                max_price = max(max_price, profit)
+            else:
+                l = r
+            r = r + 1
+        return max_price
