e9c34f3ccb2778c9b1420c9bd328e25bca3aa180
Juno BAEK <laegel1@gmail.com>
2026-01-23 15:48:18 -0800

    solve leetcode 141

---
+++ java/src/leetcode141.java
@@ -0,0 +1,35 @@
+import org.w3c.dom.NodeList;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author laegel
+ * @version 1.0
+ * @since 2026-01-19
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class leetcode141 {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet();
+        ListNode cur = head;
+
+        while (cur != null) {
+            if (set.contains(cur)) {
+                return true;
+            }
+            set.add(cur);
+            cur = cur.next;
+        }
+
+        return false;
+    }
+}
