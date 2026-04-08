2df28b743abd8da9c1f395edcb7252c30279ccc7
juno <laegel1@gmail.com>
2026-04-08 13:26:25 +0900

    programmers solve

--- java/src/programmers42577.java
+++ java/src/programmers42577.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class programmers42577 {
 
     public static void main(String[] args) {
@@ -5,22 +7,10 @@ public class programmers42577 {
     }
 
     public static boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        
         for (int i = 0; i < phone_book.length - 1; i++) {
-            for (int j = i + 1; j < phone_book.length; j++) {
-                String s1 = phone_book[i];
-                String s2 = phone_book[j];
-                int length = s1.length() > s2.length() ? s2.length() : s1.length();
-                
-                boolean flag = false;
-                for (int x = 0; x < length; x++) {
-                    if (s1.charAt(x) !=  s2.charAt(x)) {
-                        flag = true;
-                        break;
-                    }
-                }
-                
-                if (!flag) return false;
-            }
+            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
         }
         
         return true;
