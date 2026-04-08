d0974b9d91a273d1ee64ae8cb3dd0e64df9c88c1
juno <laegel1@gmail.com>
2026-04-08 12:31:28 +0900

    programmers solve

---
+++ java/src/programmers42576.java
@@ -0,0 +1,23 @@
+public class programmers42576 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String part : participant) {
+            map.put(part, map.getOrDefault(part, 0) + 1);
+        }
+        
+        for (String comp : completion) {
+            map.put(comp, map.get(comp) - 1);
+        }
+        
+        for (String key : map.keySet()) {
+            if (map.get(key) > 0) {
+                answer = key;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
