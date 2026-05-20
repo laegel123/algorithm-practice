c3ec98c99a8b5516c99845a590dc359497029c13
juno <laegel1@gmail.com>
2026-05-20 20:35:56 +0900

    leetcode solve

---
+++ java/src/leetcode28.java
@@ -0,0 +1,20 @@
+package java.src;
+
+public class leetcode28 {
+    int hLen = haystack.length();
+    int nLen = needle.length();
+
+    for (int i = 0; i <= hLen - nLen; i++) {
+        int j = 0;
+
+        while (j < nLen && haystack.charAt(i + j) == needle.charAt(j)) {
+            j++;
+        }
+
+        if (j == nLen) {
+            return i;
+        }
+    }
+
+    return -1;
+}
