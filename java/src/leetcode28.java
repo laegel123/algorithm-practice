46a60e4d5b9e2f8b4b7d23eabc7c1ba4e2b9d884
laegel <laegel1@gmail.com>
2026-05-27 09:21:42 +0900

    fix

--- java/src/leetcode28.java
+++ java/src/leetcode28.java
@@ -1,20 +1,21 @@
-package java.src;
-
 public class leetcode28 {
-    int hLen = haystack.length();
-    int nLen = needle.length();
 
-    for (int i = 0; i <= hLen - nLen; i++) {
-        int j = 0;
+    public int strStr(String haystack, String needle) {
+        int hLen = haystack.length();
+        int nLen = needle.length();
 
-        while (j < nLen && haystack.charAt(i + j) == needle.charAt(j)) {
-            j++;
+        for (int i = 0; i <= hLen - nLen; i++) {
+            int j = 0;
+
+            while (j < nLen && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+
+            if (j == nLen) {
+                return i;
+            }
         }
 
-        if (j == nLen) {
-            return i;
-        }
+        return -1;
     }
-
-    return -1;
 }
